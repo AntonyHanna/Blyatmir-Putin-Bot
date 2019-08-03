f4aa83896972a5304fa365ca7562f334d2d59120
Antony Hanna <40163981+AntonyHanna@users.noreply.github.com>
2019-08-03 21:02:12 +1000

    Added the ability to interact with server docker containers

--- src/AppEnvironment.cs
+++ src/AppEnvironment.cs
@@ -4,16 +4,22 @@ namespace Blyatmir_Putin_Bot
 {
     public class AppEnvironment
     {
-        private static string _botToken = "BOT_TOKEN";
+        private static string _botToken = "BOT_TOKEN.XSgp_A.loWkyod0iTinxAjHUHRMhZaHwzE";
         private static string _botPrefix = "BOT_PREFIX";
         private static string _configLocation = "config/";
         private static string _botActivity = "BOT_ACTIVITY";
+        private static string _dockerIP = "DOCKER_IP";
+        private static string _serverLogin = "SERVER_LOGIN";
+        private static string _serverPassword = "SERVER_PASSWORD";
 
         public static string BotToken => _botToken;
         public static string BotPrefix => _botPrefix;
         public static string ConfigLocation => _configLocation;
-
         public static string BotActivity => _botActivity;
+        public static string DockerIP => _dockerIP;
+        public static string ServerLogin => _serverLogin;
+        public static string ServerPassword => _serverPassword;
+
 
         /// <summary>
         /// Load environment variables
@@ -24,6 +30,9 @@ namespace Blyatmir_Putin_Bot
             string botToken = Environment.GetEnvironmentVariable("BOT_TOKEN");
             string botPrefix = Environment.GetEnvironmentVariable("BOT_PREFIX");
             string botActivity = Environment.GetEnvironmentVariable("BOT_ACTIVITY");
+            string dockerIP = Environment.GetEnvironmentVariable("DOCKER_IP");
+            string serverLogin = Environment.GetEnvironmentVariable("SERVER_LOGIN");
+            string serverPassword = Environment.GetEnvironmentVariable("SERVER_PASSWORD");
 
             //if the above values are not null and Bot_Token isn't default value
             //assign value
@@ -37,6 +46,14 @@ namespace Blyatmir_Putin_Bot
             if (!string.IsNullOrWhiteSpace(botActivity) && BotActivity == "BOT_ACTIVITY")
                 _botActivity = botActivity;
 
+            if (!string.IsNullOrWhiteSpace(dockerIP) && DockerIP == "DOCKER_IP")
+                _dockerIP = dockerIP;
+
+            if (!string.IsNullOrWhiteSpace(serverLogin) && DockerIP == "SERVER_LOGIN")
+                _serverLogin = serverLogin;
+
+            if (!string.IsNullOrWhiteSpace(serverPassword) && DockerIP == "SERVER_PASSWORD")
+                _serverPassword = serverPassword;
         }
     }
 }
