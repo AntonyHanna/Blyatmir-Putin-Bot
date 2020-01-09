d38b33e60fe7c1a5065463b0f2babe42cabe7c55
Antony Hanna <40163981+AntonyHanna@users.noreply.github.com>
2020-01-09 17:08:11 +1100

    Respond to F
    Now responds to lone `F`'s also now has a cooldown on how often he will respond with F (30 Second cooldown)

--- src/Services/FInChatService.cs
+++ src/Services/FInChatService.cs
@@ -1,10 +1,13 @@
 ﻿using Discord.WebSocket;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Blyatmir_Putin_Bot.Services
 {
 	public class FInChatService
 	{
+		private static bool onCooldown = false;
+
 		/// <summary>
 		/// Checks if the message should recieve an F
 		/// </summary>
@@ -14,16 +17,34 @@ namespace Blyatmir_Putin_Bot.Services
 		{
 			string[] terms = new string[] { "F", "in", "chat", "respect", "respects", "assignment", "due", "fucked", "can", "get", "bois" };
 			int fCounter = 0;
+			bool skipCheck = false;
+
+			Timer timer = new Timer
+			{
+				AutoReset = true,
+				Interval = 30000,
+			};
+
+			timer.Elapsed += ResetCooldown;
+			timer.Enabled = true;
 
 			foreach (string str in terms)
-				if (message.Content.Contains(str))
-					fCounter++;
+			{
+				if(message.Content.Contains("F", System.StringComparison.OrdinalIgnoreCase))
+					skipCheck = true;
 
-			if (fCounter >= 2 && !message.Author.IsBot)
+				else if (message.Content.Contains(str, System.StringComparison.OrdinalIgnoreCase))
+					fCounter++;
+			}
+
+			if ((fCounter >= 2 && !message.Author.IsBot && !onCooldown) || (skipCheck && !onCooldown))
 			{
 				fCounter = 0;
+				onCooldown = true;
 				await message.Channel.SendMessageAsync(text: "F");
 			}
 		}
+
+		private static void ResetCooldown(object sender, ElapsedEventArgs e) => onCooldown = false;
 	}
 }
