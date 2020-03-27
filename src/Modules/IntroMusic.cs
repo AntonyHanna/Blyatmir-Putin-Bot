1d492e89b6cbf2786fcdd2fe48a96336a9260b73
Antony Hanna <40163981+AntonyHanna@users.noreply.github.com>
2020-03-28 01:30:35 +1100

    Basic Implementation of intro music uploading

--- src/Modules/IntroMusic.cs
+++ src/Modules/IntroMusic.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using Blyatmir_Putin_Bot.Model;
+using Discord;
 using Discord.Commands;
 
 namespace Blyatmir_Putin_Bot.Modules
@@ -11,6 +15,7 @@ namespace Blyatmir_Putin_Bot.Modules
 		// enabling and selecting song on user basis
 		// 
 		[Command("set")]
+		[Alias("-s")]
 		public async Task SetIntroMusic([Remainder] string songName)
 		{
 			User userData = User.GetUser(Context.Message.Author.Id);
@@ -21,6 +26,7 @@ namespace Blyatmir_Putin_Bot.Modules
 		}
 
 		[Command("remove")]
+		[Alias("-r")]
 		public async Task RemoveIntroMusic()
 		{
 			User userData = User.GetUser(Context.Message.Author.Id);
@@ -28,6 +34,32 @@ namespace Blyatmir_Putin_Bot.Modules
 			User.Write(User.UserList);
 
 			await Context.Channel.SendMessageAsync($"Intro Music for `{Context.Message.Author.Username}` has been removed");
+		}
+
+
+		[Command("upload")]
+		[Alias("-u")]
+		public async Task UploadIntroMusic()
+		{
+			Attachment attachment = Context.Message.Attachments.First();
+			if(File.Exists($"{AppEnvironment.ConfigLocation}/resources/introMusic/{attachment.Filename}"))
+			{
+				// Send Embed and return
+				await Context.Channel.SendMessageAsync("File already exists with this name");
+				return;
+			}
+
+			if(!attachment.Filename.Contains(".mp3"))
+			{
+				// Send embed and return
+				await Context.Channel.SendMessageAsync("File is of the wrong type, I only accept \".mp3\" files because tony is lazy");
+				return;
+			}
+			using(WebClient client = new WebClient())
+			{
+				client.DownloadFileAsync(new System.Uri(attachment.Url), $"{AppEnvironment.ConfigLocation}/resources/introMusic/{attachment.Filename}");
+				await Context.Channel.SendMessageAsync($"File: `{attachment.Filename}` has been uploaded");
+			}
 		}
 	}
 }
