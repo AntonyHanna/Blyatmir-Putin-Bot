c150b5b7bf84a7637d037dd9432276f1a7056fea
Antony Hanna <40163981+AntonyHanna@users.noreply.github.com>
2020-08-17 20:43:18 +1000

    Removed DiceRollAsync(int, int) overload
    Removed the DiceRollAsync(int, int) overload since it isn't used

--- blyatmir-putin/Modules/Dice.cs
+++ blyatmir-putin/Modules/Dice.cs
@@ -70,55 +70,5 @@ namespace Blyatmir_Putin_Bot.Modules
 			//send the message
 			await Context.Channel.SendMessageAsync(embed: easyEmbed.Build());
 		}
-
-		/// <summary>
-		/// Roll a dice from num1 to num2
-		/// </summary>
-		/// <param name="num1"></param>
-		/// <param name="num2"></param>
-		/// <returns></returns>
-		[Command("roll")]
-		[Alias("dice")]
-		public async Task DiceRollAsync(int num1, int num2)
-		{
-			//invalid number check
-			if (num1 > num2)
-			{
-				//you get the point
-				var failedEmbed = new EasyEmbed()
-				{
-					AuthorName = "(╯°□°）╯︵ ┻━┻",
-					AuthorIcon = $"https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/153/game-die_1f3b2.png",
-					EmbedColor = Color.Red,
-					EmbedTitle = $"Dice Roll Failed",
-					FooterText = $"Your first number should be smaller than your second number"
-				};
-
-				//same thing as always
-				await Context.Channel.SendMessageAsync("", false, failedEmbed.Build(), null);
-				return;
-			}
-
-			//ya its the thing you've seen like 100 times already
-			var field = new EmbedFieldBuilder
-			{
-				Name = $"Roll",
-				Value = $"`{new Random().Next(num1, num2)}`",
-			};
-
-			//wow templates, cool
-			var successfulEmbed = new EasyEmbed()
-			{
-				AuthorName = "(╯°□°）╯︵ ┻━┻",
-				AuthorIcon = $"https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/153/game-die_1f3b2.png",
-				EmbedColor = Color.Green,
-				EmbedTitle = $"Dice Roll Results",
-				EmbedField = field,
-				FooterText = $"Rolled on some mystical dice from bum fuck nowhere seems to have {num2 - num1 + 1} possible value(s)... BLYAT!"
-			};
-
-			//sending the damn message
-			await Context.Channel.SendMessageAsync("", false, successfulEmbed.Build(), null);
-		}
 	}
 }
