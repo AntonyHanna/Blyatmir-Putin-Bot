b7b7d4a043f510f2e3b3ae78a328da3637c3b873
Antony Hanna <antonyhanna01@gmail.com>
2020-10-23 21:05:19 +1100

    Added settings interface

---
+++ blyatmir-putin/Model/IAppConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blyatmir_Putin_Bot.Model
+{
+	public interface IAppConfiguration
+	{
+		public string Token { get; set; }
+		public string Prefix { get; set; }
+		public string RootDirectory { get; set; }
+		public string Activity { get; set; }
+		public string DockerIP { get; set; }
+		public string ServerUser { get; set; }
+		public string ServerPassword { get; set; }
+	}
+}
