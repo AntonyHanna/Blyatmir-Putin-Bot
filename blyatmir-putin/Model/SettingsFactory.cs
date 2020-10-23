aef0a122e1fd7eb705578121c12a5b6721bd4a6b
Antony Hanna <antonyhanna01@gmail.com>
2020-10-23 21:05:52 +1100

    Added settings factory

---
+++ blyatmir-putin/Model/SettingsFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Blyatmir_Putin_Bot.Model
+{
+	public static class SettingsFactory
+	{
+		public static bool IsDocker
+		{
+			get
+			{
+				if (Environment.GetEnvironmentVariable("ISDOCKER") != null)
+				{
+					return true;
+				}
+
+				return false;
+			}
+		}
+
+		public static IAppConfiguration Create()
+		{
+			if(SettingsFactory.IsDocker)
+			{
+				return new EnvironmentSettings();
+			}
+			else
+			{
+				LocalSettings settings = new LocalSettings();
+				settings.LoadSettings();
+
+				return settings;
+			}
+		}
+	}
+}
