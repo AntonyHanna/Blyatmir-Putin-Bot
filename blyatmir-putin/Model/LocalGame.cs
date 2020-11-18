575cde4ef5f25b1a0919d36a735de55f882ae90d
Antony Hanna <antonyhanna01@gmail.com>
2020-11-18 18:03:21 +1100

    Added ElCheapo Game wrapper

---
+++ blyatmir-putin/Model/LocalGame.cs
@@ -0,0 +1,22 @@
+﻿using ElCheapo.Generics;
+using System;
+
+namespace Blyatmir_Putin_Bot.Model
+{
+	public class LocalGame : Game
+	{
+		public bool Posted { get; set; }
+
+		public LocalGame() : base() { }
+
+		public LocalGame(Game game) : base(game.EffectiveDate, game.Name, game.Description, game.Developer, game.Publisher)
+		{
+			this.Posted = false;
+		}
+
+		public LocalGame(DateTime effectiveDate, string name, string description, string developer, string publisher) : base(effectiveDate, name, description, developer, publisher)
+		{
+			this.Posted = false;
+		}	
+	}
+}
