d82cd020e244c3d34dbbc086ba5ce387093e8222
Antony Hanna <antonyhanna01@gmail.com>
2020-11-25 23:26:00 +1100

    Added a way to load all custom attributes

---
+++ blyatmir-putin/Model/AttributeLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Blyatmir_Putin_Bot.Model
+{
+	public static class AttributeLoader
+	{
+		public static void LoadCustomAttributes()
+		{
+			Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+
+			foreach (Type type in types)
+			{
+				IEnumerable<string> dir = type.GetCustomAttributes(typeof(DirectoryRequiredAttribute), true).Cast<DirectoryRequiredAttribute>().Select(x => x.DirectoryPath);
+			}
+		}
+	}
+}
