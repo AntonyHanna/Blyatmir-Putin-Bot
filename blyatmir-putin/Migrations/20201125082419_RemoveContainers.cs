f16d860c8cc0c6f20c0ad5278014252ee5007f34
Antony Hanna <antonyhanna01@gmail.com>
2020-11-25 19:26:29 +1100

    Migrate the DB to not include Containers

---
+++ blyatmir-putin/Migrations/20201125082419_RemoveContainers.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blyatmir_Putin_Bot.Migrations
+{
+    public partial class RemoveContainers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Containers");
+
+            migrationBuilder.DropColumn(
+                name: "ContainerAccessLevel",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ContainerAccessLevel",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Containers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Image = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    PermisssionLevel = table.Column<int>(type: "INTEGER", nullable: false),
+                    Tag = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Containers", x => x.Id);
+                });
+        }
+    }
+}
