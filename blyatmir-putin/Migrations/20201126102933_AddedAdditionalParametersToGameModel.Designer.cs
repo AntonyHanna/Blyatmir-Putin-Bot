9f898adfe2f6d379fa28143a1be1226bda63407f
Antony Hanna <antonyhanna01@gmail.com>
2020-11-26 22:33:29 +1100

    Added a new migration to support the new properties of Game

--- blyatmir-putin/Migrations/20201126102933_AddedAdditionalParametersToGameModel.Designer.cs
+++ blyatmir-putin/Migrations/20201126102933_AddedAdditionalParametersToGameModel.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Blyatmir_Putin_Bot.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Blyatmir_Putin_Bot.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20201126102933_AddedAdditionalParametersToGameModel")]
+    partial class AddedAdditionalParametersToGameModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ blyatmir-putin/Migrations/20201126102933_AddedAdditionalParametersToGameModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blyatmir_Putin_Bot.Migrations
+{
+    public partial class AddedAdditionalParametersToGameModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EffectiveDate",
+                table: "Games",
+                newName: "StartDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BannerUri",
+                table: "Games",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Games",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "PosterUri",
+                table: "Games",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BannerUri",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "PosterUri",
+                table: "Games");
+
+            migrationBuilder.RenameColumn(
+                name: "StartDate",
+                table: "Games",
+                newName: "EffectiveDate");
+        }
+    }
+}
