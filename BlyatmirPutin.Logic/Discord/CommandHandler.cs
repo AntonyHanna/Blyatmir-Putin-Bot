68529cdb028bb12da627d543614b7811ad4573d8
Antony Hanna <antonyhanna01@gmail.com>
2021-12-26 13:43:34 +1100

    Added Discord template functionality

---
+++ BlyatmirPutin.Logic/Discord/CommandHandler.cs
@@ -0,0 +1,64 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlyatmirPutin.Logic.Discord
+{
+	internal class CommandHandler
+	{
+		private DiscordSocketClient Client { get; set; }
+
+		private CommandService Commands { get; set; }
+
+		public CommandHandler(DiscordSocketClient client, CommandService commands)
+		{
+            this.Client = client;
+            this.Commands = commands;
+		}
+
+        public async Task InstallCommandsAsync()
+		{
+            Client.MessageReceived += HandleCommandAsync;
+
+            await Commands.AddModulesAsync(
+                assembly: Assembly.GetExecutingAssembly(),
+                services: null
+            );
+		}
+
+        public async Task HandleCommandAsync(SocketMessage arg)
+        {
+            // Bail out if it's a System Message.
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+
+            // We don't want the bot to respond to itself or other bots.
+            if (msg.Author.Id == Client?.CurrentUser.Id || msg.Author.IsBot) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int pos = 0;
+
+            // get the guild based prefix
+
+            if (msg.HasCharPrefix('/', ref pos))
+            {
+                // Create a Command Context.
+                var context = new SocketCommandContext(Client, msg);
+
+                // Execute the command. (result does not indicate a return value, 
+                // rather an object stating if the command executed successfully).
+                var result = await Commands.ExecuteAsync(context: context, argPos: pos, null);
+
+#if DEBUG
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+					await msg.Channel.SendMessageAsync(result.ErrorReason);
+#endif
+            }
+        }
+    }
+}
---
+++ BlyatmirPutin.Logic/Discord/DiscordManager.cs
@@ -0,0 +1,84 @@
+﻿using BlyatmirPutin.Common.Logging;
+using BlyatmirPutin.Models.Interfaces;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace BlyatmirPutin.Logic.Discord
+{
+	public class DiscordManager : IDisposable
+	{
+		/// <summary>
+		/// Represents the discord bot client
+		/// </summary>
+		/// <remarks>Will be null until <see cref="ConnectAsync(IConfiguration?)"/> is called</remarks>
+		public DiscordSocketClient? Client { get; private set; }
+
+		public CommandService? CommandService{ get; private set; }
+
+		public async Task ConnectAsync(IConfiguration? config)
+		{
+			Client = new DiscordSocketClient(new DiscordSocketConfig
+			{
+				GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildVoiceStates | GatewayIntents.GuildMessages,
+				LogLevel = LogSeverity.Debug,
+				UseSystemClock = true
+			});
+
+			CommandService = new CommandService(new CommandServiceConfig
+			{ 
+				LogLevel = LogSeverity.Debug,
+				CaseSensitiveCommands = true
+			});
+
+			Client.Log += Client_Log;
+			CommandService.Log += Client_Log;
+
+			CommandHandler handler = new CommandHandler(Client, CommandService);
+
+			await handler.InstallCommandsAsync();
+			await Client.LoginAsync(TokenType.Bot, config?.Token);
+			await Client.SetGameAsync(config?.Activity);
+			await Client.StartAsync();
+		}
+
+		public async Task DisconnectAsync()
+		{
+			if(Client != null)
+				await Client.StopAsync();
+		}
+
+		private Task Client_Log(LogMessage arg)
+		{
+			switch(arg.Severity)
+			{
+				case LogSeverity.Info:
+				case LogSeverity.Verbose:
+					Logger.LogInfo(arg.Message, arg.Source);
+					break;
+
+				case LogSeverity.Debug:
+					Logger.LogDebug(arg.Message, arg.Source);
+					break;
+
+				case LogSeverity.Warning:
+					Logger.LogWarning(arg.Message, arg.Source);
+					break;
+
+				case LogSeverity.Error:
+					Logger.LogError(arg.Message, arg.Source);
+					break;
+
+				case LogSeverity.Critical:
+					Logger.LogCritical(arg.Message, arg.Source);
+					break;
+			}
+			return Task.CompletedTask;
+		}
+
+		public void Dispose()
+		{
+			Client?.Dispose();
+		}
+	}
+}
