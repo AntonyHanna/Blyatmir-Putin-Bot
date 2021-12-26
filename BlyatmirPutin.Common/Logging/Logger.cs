70bbb428a50cd5b80883929d866f0dd289f811e4
Antony Hanna <antonyhanna01@gmail.com>
2021-12-26 13:40:05 +1100

    Added simple logging project
    Will probably replace this later on

---
+++ BlyatmirPutin.Common/Logging/Logger.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace BlyatmirPutin.Common.Logging
+{
+	public static class Logger
+	{
+		private static bool IsConsoleAvailable
+		{
+			get
+			{
+				return Console.LargestWindowWidth != 0;
+			}
+		}
+
+		public static void LogInfo(string message, [CallerMemberName] string callerName = "")
+		{
+			if(IsConsoleAvailable)
+				Console.WriteLine(new MessageLog(message, MessageLog.LogLevel.Info, DetermineForegroundColor(), callerName));
+		}
+
+		public static void LogDebug(string message, [CallerMemberName] string callerName = "")
+		{
+			if (IsConsoleAvailable)
+				Console.WriteLine(new MessageLog(message, MessageLog.LogLevel.Debug, ConsoleColor.Green, callerName));
+		}
+
+		public static void LogWarning(string message, [CallerMemberName] string callerName = "")
+		{
+			if (IsConsoleAvailable)
+				Console.WriteLine(new MessageLog(message, MessageLog.LogLevel.Warning, ConsoleColor.Yellow, callerName));
+		}
+
+		public static void LogError(string message, [CallerMemberName] string callerName = "")
+		{
+			if (IsConsoleAvailable)
+				Console.Error.WriteLine(new MessageLog(message, MessageLog.LogLevel.Error, ConsoleColor.Red, callerName));
+		}
+
+		public static void LogCritical(string message, [CallerMemberName] string callerName = "")
+		{
+			if (IsConsoleAvailable)
+				Console.Error.WriteLine(new MessageLog(message, MessageLog.LogLevel.Critical, ConsoleColor.Magenta, callerName));
+		}
+		
+		private static ConsoleColor DetermineForegroundColor()
+		{
+			if(Console.BackgroundColor == ConsoleColor.Black)
+			{
+				return ConsoleColor.White;
+			}
+
+			return ConsoleColor.Black;
+		}
+	}
+}
---
+++ BlyatmirPutin.Common/Logging/MessageLog.cs
@@ -0,0 +1,48 @@
+﻿namespace BlyatmirPutin.Common.Logging
+{
+	public class MessageLog
+	{
+		private static DateTime _timestamp;
+		private static DateTime Timestamp
+		{
+			get
+			{
+				if (_timestamp == default)
+				{
+					_timestamp = DateTime.Now;
+				}
+
+				return _timestamp;
+			}
+		}
+
+		public string Message { get; set; }
+		public LogLevel Severity { get; set; }
+
+		public MessageLog(string message, LogLevel severity, ConsoleColor color, string callerName = "")
+		{
+			this.Severity = severity;
+			this.Message = FormatMessage(message, color, severity.ToString(), callerName);
+		}
+
+		private static string FormatMessage(string message, ConsoleColor color = default, string logLevel = "", string callerName = "")
+		{
+			Console.ForegroundColor = color;
+			return string.Format($"{Timestamp,-10:dd MMM yyyy - hh:mm tt}  {logLevel,-8}  {callerName,-10}  {message}", message);
+		}
+
+		public enum LogLevel
+		{
+			Info,
+			Debug,
+			Warning,
+			Error,
+			Critical
+		}
+
+		public override string ToString()
+		{
+			return Message;
+		}
+	}
+}
