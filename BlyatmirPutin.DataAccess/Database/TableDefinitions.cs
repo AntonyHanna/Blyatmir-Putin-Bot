12443392f6b02409727c7a1d012cd989f6ba7da2
Antony Hanna <antonyhanna01@gmail.com>
2021-12-26 14:57:36 +1100

    Fix table definitions

--- BlyatmirPutin.DataAccess/Database/TableDefinitions.cs
+++ BlyatmirPutin.DataAccess/Database/TableDefinitions.cs
@@ -16,8 +16,8 @@ namespace BlyatmirPutin.DataAccess.Database
 			{ typeof(Guild), "CREATE TABLE Guild (Id BIGINT, Prefix char DEFAULT '!', PRIMARY KEY(Id));" },
 			{ typeof(Member), "CREATE TABLE Member (Id BIGINT, CurrentIntro INT, PRIMARY KEY (Id));" },
 			{ typeof(Game), "CREATE TABLE Game (Id INT IDENTITY, WasPosted BIT DEFAULT 0, Title NVARCHAR NOT NULL, Blurb NVARCHAR, Developer NVARCHAR, Publisher NVARCHAR, Banner NVARCHAR, Poster NVARCHAR, StartDate DATETIME NOT NULL, EndDate DATETIME NOT NULL, PRIMARY KEY (Id));" },
-			{ typeof(IntroMusic), "CREATE TABLE IntroMusic (Id INT IDENTITY, UploaderId BIGINT NOT NULL, IntroName NVARCHAR NOT NULL, FilePath NVARCHAR NOT NULL, DateAdded DATETIME DEFAULT GETDATE(), PRIMARY KEY (Id));" },
-			{ typeof(IntroMusicRecord), "CREATE TABLE IntroMusicModule (Id INT IDENTITY, GuildId BIGINT, IsEnabled BIT DEFAULT 1, PRIMARY KEY (Id));" },
+			{ typeof(IntroMusic), "CREATE TABLE IntroMusic (Id INT IDENTITY, UploaderId BIGINT NOT NULL, IntroName NVARCHAR NOT NULL, FilePath NVARCHAR NOT NULL, DateAdded DATETIME DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (Id));" },
+			{ typeof(IntroMusicRecord), "CREATE TABLE IntroMusicRecord (Id INT IDENTITY, UserId BIGINT, IntroId INT, DateSet DATETIME, DateUnset DATETIME, PRIMARY KEY (Id));" },
 			{ typeof(FModule), "CREATE TABLE FModule (Id INT IDENTITY, GuildId BIGINT, IsEnabled BIGINT default 1, Threshold INT DEFAULT 3, Cooldown INT DEFAULT 30, PRIMARY KEY (Id));" },
 			{ typeof(GameNotifierModule), "CREATE TABLE GameNotifierModule (Id INT IDENTITY, GuildId BIGINT, IsEnabled BIGINT DEFAULT 1, PRIMARY KEY (Id));" },
 			{ typeof(IntroMusicModule), "CREATE TABLE IntroMusicModule (Id INT IDENTITY, GuildId BIGINT, IsEnabled BIT DEFAULT 1, PRIMARY KEY (Id));" }
