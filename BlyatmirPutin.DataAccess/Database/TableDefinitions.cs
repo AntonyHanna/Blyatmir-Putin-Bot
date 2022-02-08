878c1f66c3f5060eb4243fb5e6192333f69c7293
Antony Hanna <antonyhanna01@gmail.com>
2022-02-08 21:21:43 +1100

    Use UNSIGNED BIGINT for Discord Ids

--- BlyatmirPutin.DataAccess/Database/TableDefinitions.cs
+++ BlyatmirPutin.DataAccess/Database/TableDefinitions.cs
@@ -14,15 +14,15 @@ namespace BlyatmirPutin.DataAccess.Database
 		/// </summary>
 		public static readonly Dictionary<Type, string> Mappings = new Dictionary<Type, string>()
 		{
-			{ typeof(Guild), "CREATE TABLE Guild (Id BIGINT, Prefix char DEFAULT '!', PRIMARY KEY(Id));" },
-			{ typeof(Member), "CREATE TABLE Member (Id BIGINT, CurrentIntro NVARCHAR, PRIMARY KEY (Id));" },
+			{ typeof(Guild), "CREATE TABLE Guild (Id UNSIGNED BIGINT, Prefix char DEFAULT '!', PRIMARY KEY(Id));" },
+			{ typeof(Member), "CREATE TABLE Member (Id UNSIGNED BIGINT, CurrentIntro NVARCHAR, PRIMARY KEY (Id));" },
 			{ typeof(Game), "CREATE TABLE Game (Id INT IDENTITY, WasPosted BIT DEFAULT 0, Title NVARCHAR NOT NULL, Blurb NVARCHAR, Developer NVARCHAR, Publisher NVARCHAR, Banner NVARCHAR, Poster NVARCHAR, StartDate DATETIME NOT NULL, EndDate DATETIME NOT NULL, PRIMARY KEY (Id));" },
-			{ typeof(IntroMusic), "CREATE TABLE IntroMusic (Id NVARCHAR IDENTITY, UploaderId BIGINT NOT NULL, IntroName NVARCHAR NOT NULL, FilePath NVARCHAR NOT NULL, DateAdded BIGINT, PRIMARY KEY (Id));" },
-			{ typeof(IntroMusicRecord), "CREATE TABLE IntroMusicRecord (Id NVARCHAR IDENTITY, UserId BIGINT, IntroId NVARCHAR, DateSet BIGINT, PRIMARY KEY (Id));" },
-			{ typeof(FModuleSettings), "CREATE TABLE FModuleSettings (Id INT IDENTITY, GuildId BIGINT, IsEnabled BIGINT default 1, Threshold INT DEFAULT 3, Cooldown INT DEFAULT 30, PRIMARY KEY (Id));" },
-			{ typeof(GameNotifierModuleSettings), "CREATE TABLE GameNotifierModuleSettings (Id INT IDENTITY, GuildId BIGINT, IsEnabled BIGINT DEFAULT 1, PRIMARY KEY (Id));" },
-			{ typeof(IntroMusicModuleSettings), "CREATE TABLE IntroMusicModuleSettings (Id NVARCHAR IDENTITY, GuildId BIGINT, IsEnabled BIT DEFAULT 1, VoteThreshold INT, PRIMARY KEY (Id));" },
-			{ typeof(IntroMusicVote), "CREATE TABLE IntroMusicVote (Id NVARCHAR IDENTITY, VoterID BIGINT, TargetUserID BIGINT, GuildID BIGINT, PRIMARY KEY (Id));" }
+			{ typeof(IntroMusic), "CREATE TABLE IntroMusic (Id NVARCHAR IDENTITY, UploaderId UNSIGNED BIGINT NOT NULL, IntroName NVARCHAR NOT NULL, FilePath NVARCHAR NOT NULL, DateAdded BIGINT, PRIMARY KEY (Id));" },
+			{ typeof(IntroMusicRecord), "CREATE TABLE IntroMusicRecord (Id NVARCHAR IDENTITY, UserId UNSIGNED BIGINT, IntroId NVARCHAR, DateSet BIGINT, PRIMARY KEY (Id));" },
+			{ typeof(FModuleSettings), "CREATE TABLE FModuleSettings (Id INT IDENTITY, GuildId UNSIGNED BIGINT, IsEnabled BIGINT default 1, Threshold INT DEFAULT 3, Cooldown INT DEFAULT 30, PRIMARY KEY (Id));" },
+			{ typeof(GameNotifierModuleSettings), "CREATE TABLE GameNotifierModuleSettings (Id INT IDENTITY, GuildId UNSIGNED BIGINT, IsEnabled BIGINT DEFAULT 1, PRIMARY KEY (Id));" },
+			{ typeof(IntroMusicModuleSettings), "CREATE TABLE IntroMusicModuleSettings (Id NVARCHAR IDENTITY, GuildId UNSIGNED BIGINT, IsEnabled BIT DEFAULT 1, VoteThreshold INT, PRIMARY KEY (Id));" },
+			{ typeof(IntroMusicVote), "CREATE TABLE IntroMusicVote (Id NVARCHAR IDENTITY, VoterID UNSIGNED BIGINT, TargetUserID UNSIGNED BIGINT, GuildID UNSIGNED BIGINT, PRIMARY KEY (Id));" }
 		};
 	}
 }
