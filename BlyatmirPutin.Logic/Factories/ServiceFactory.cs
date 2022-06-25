8e040a1ab0f05ee0ed17d7fab981689053b9320a
Antony Hanna <antonyhanna01@gmail.com>
2022-06-25 16:37:08 +1000

    Added temporary ServiceFactory
    Its not really a factory... but it works for now

---
+++ BlyatmirPutin.Logic/Factories/ServiceFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace BlyatmirPutin.Logic.Factories
+{
+	public static class ServiceFactory
+	{
+		private static ServiceProvider? _serviceProvider;
+		public static IServiceProvider? ServiceProvider
+		{
+			get
+			{
+				if(_serviceProvider == null)
+				{
+					_serviceProvider = new ServiceCollection().BuildServiceProvider();
+				}
+
+				return _serviceProvider;
+			}
+		}
+	}
+}
